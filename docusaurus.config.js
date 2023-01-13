35b85b6df3e7818e95c6b018c34e61b011f17ac6
snehangshuk <snehangshu@gmail.com>
2023-01-13 10:26:38 -0800

    1 file modified

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -101,7 +101,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Snehangshu Karmakar Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
