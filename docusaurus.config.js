356b6af14444a3df811061761648ca530b220ab4
snehangshuk <snehangshu@gmail.com>
2023-01-13 11:42:44 -0800

    1 file modified

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Using Docusaurus and its  Cool Features',
+  title: 'Using Docusaurus and its Cool Features',
   tagline: 'Dinosaurs are cool',
   url: 'https://snehangshuk.github.io/',
   baseUrl: '/docusaurus-test/',
@@ -39,7 +39,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',  
+            'https://github.com/snehangshuk/docusaurus-test',  
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -65,7 +65,7 @@ const config = {
             label: 'Tutorial',
           },*/
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/snehangshuk/docusaurus-test',
             label: 'GitHub',
             position: 'right',
           },
@@ -96,7 +96,7 @@ const config = {
             items: [
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/snehangshuk/docusaurus-test',
               },
             ],
           },
