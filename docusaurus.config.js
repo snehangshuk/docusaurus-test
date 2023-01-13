e3d15d252a807129c9017deb6839ea435b61d6e1
snehangshuk <snehangshu@gmail.com>
2023-01-12 17:17:21 -0800

    1 file modified

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Using Docusaurus and its Cool Features',
+  title: 'Using Docusaurus and its  Cool Features',
   tagline: 'Dinosaurs are cool',
   url: 'https://snehangshuk.github.io/',
   baseUrl: '/docs/',
